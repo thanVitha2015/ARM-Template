7d19369f830891057161b287a802b6ccaa76f355
Prashanth Jayaram <powershellsql@gmail.com>
2024-01-21 09:40:08 -0500

    Add files via upload

---
+++ Chapter 11 - Azure DevOps - PowerShell Script.ps1
@@ -0,0 +1,11 @@
+﻿    param(
+        [Parameter(Mandatory=$true)]
+        [int]$Number1,
+
+        [Parameter(Mandatory=$true)]
+        [int]$Number2
+    )
+
+    $Sum = $Number1 + $Number2
+    Write-Host "Sum of $Number1 and $Number2 is: $Sum"
+
